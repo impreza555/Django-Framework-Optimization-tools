0f81774ab53a0553502b6c227b5dd25a34d2a94e
impreza555 <alex.erjomin@outlook.com>
2022-02-17 19:53:48 +0300

    Подключил бекенд memcached.

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -233,3 +233,15 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.user.user_details',
     'authapp.pipeline.save_user_profile',
 )
+
+if os.name == 'posix':
+    CACHE_MIDDLEWARE_ALIAS = 'default'
+    CACHE_MIDDLEWARE_SECONDS = 120
+    CACHE_MIDDLEWARE_KEY_PREFIX = 'geekshop'
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+            'LOCATION': '127.0.0.1:11211',
+        }
+    }
+LOW_CACHE = True
