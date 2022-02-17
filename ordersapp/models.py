fce2147c4d43d325f16c1f4d2fd836ee97d28bf3
impreza555 <alex.erjomin@outlook.com>
2022-02-17 18:55:38 +0300

    Подключил cached_property и кеширование в шаблоне при помощи with.

--- ordersapp/models.py
+++ ordersapp/models.py
@@ -32,17 +32,22 @@ class Order(models.Model):
     def __str__(self):
         return f'Текущий заказ: {self.pk}'
 
-    def get_total_quantity(self):
-        items = self.orderitems.select_related('product')
-        return sum(list(map(lambda x: x.quantity, items)))
+    def get_summary(self):
+        items = self.orderitems.select_related()
+        return {'total_cost': sum(list(map(lambda x: x.quantity * x.product.price, items))),
+                'total_quantity': sum(list(map(lambda x: x.quantity, items)))}
+
+    # def get_total_quantity(self):
+    #     items = self.orderitems.select_related('product')
+    #     return sum(list(map(lambda x: x.quantity, items)))
 
     def get_product_type_quantity(self):
         items = self.orderitems.select_related()
         return len(items)
 
-    def get_total_cost(self):
-        items = self.orderitems.select_related('product')
-        return sum(list(map(lambda x: x.quantity * x.product.price, items)))
+    # def get_total_cost(self):
+    #     items = self.orderitems.select_related('product')
+    #     return sum(list(map(lambda x: x.quantity * x.product.price, items)))
 
     def delete(self):
         for item in self.orderitems.select_related('product'):
