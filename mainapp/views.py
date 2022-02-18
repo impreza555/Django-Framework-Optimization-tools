c18be4324b7cfe2139965f5602a89d98260f9740
impreza555 <alex.erjomin@outlook.com>
2022-02-18 20:05:36 +0300

    Настроил использование низкоуровневого кеширования с помощью memcached.

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,7 @@
 import os
 
+from django.conf import settings
+from django.core.cache import cache
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render
 from django.views.generic import DetailView
@@ -14,16 +16,61 @@ def index(request):
     return render(request, 'mainapp/index.html', context)
 
 
+def get_links_menu():
+    if settings.LOW_CACHE:
+        key = 'links_menu'
+        links_menu = cache.get(key)
+        if links_menu is None:
+            links_menu = ProductCategory.objects.filter(is_active=True)
+            cache.set(key, links_menu)
+        return links_menu
+    else:
+        return ProductCategory.objects.filter(is_active=True)
+
+
+def get_products(category_pk=None):
+    if settings.LOW_CACHE:
+        if category_pk:
+            key = f'category_{category_pk}'
+            products = cache.get(key)
+            if products is None:
+                products = Product.objects.filter(is_active=True, category__is_active=True,
+                                                  category_id=category_pk).select_related('category')
+                cache.set(key, products)
+            return products
+        else:
+            key = 'products'
+            products = cache.get(key)
+            if products is None:
+                products = Product.objects.filter(is_active=True, category__is_active=True).select_related('category')
+                cache.set(key, products)
+            return products
+    else:
+        if category_pk:
+            return Product.objects.filter(is_active=True, category__is_active=True,
+                                          category_id=category_pk).select_related('category')
+        else:
+            return Product.objects.filter(is_active=True, category__is_active=True).select_related('category')
+
+
+def get_product_one(pk):
+    if settings.LOW_CACHE:
+        key = f'product_{pk}'
+        product = cache.get(key)
+        if product is None:
+            product = Product.objects.get(pk=pk)
+            cache.set(key, product)
+        return product
+    else:
+        return Product.objects.get(pk=pk)
+
+
 def products(request, id_category=None, page=1):
     context = {
         'title': 'Geekshop | Каталог',
     }
 
-    if id_category:
-        products = Product.objects.filter(category_id=id_category).select_related('category')
-    else:
-        products = Product.objects.all().select_related()
-
+    products = get_products(id_category)
     paginator = Paginator(products, per_page=3)
 
     try:
@@ -34,7 +81,7 @@ def products(request, id_category=None, page=1):
         products_paginator = paginator.page(paginator.num_pages)
 
     context['products'] = products_paginator
-    context['categories'] = ProductCategory.objects.all()
+    context['categories'] = get_links_menu()
     return render(request, 'mainapp/products.html', context)
 
 
@@ -45,8 +92,7 @@ class ProductDetail(DetailView):
     model = Product
     template_name = 'mainapp/detail.html'
 
-    # def get_context_data(self, **kwargs):
-    #     context = super(ProductDetail, self).get_context_data(**kwargs)
-    #     product = self.get_object()
-    #     context['product'] = product
-    #     return context
+    def get_context_data(self, **kwargs):
+        context = super(ProductDetail, self).get_context_data(**kwargs)
+        context['product'] = get_product_one(self.kwargs.get('pk'))
+        return context
