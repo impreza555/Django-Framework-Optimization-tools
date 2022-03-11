0bc06271fba78106c9e256894c279a9d9d1972dc
impreza555 <alex.erjomin@outlook.com>
2022-03-11 20:07:09 +0300

    Доделал авторизацию через Google (сохранение фото пользователя и языка в профиль).

--- baskets/views.py
+++ baskets/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect, JsonResponse
-# from django.shortcuts import render
 from django.template.loader import render_to_string
 from baskets.models import Basket
 from mainapp.models import Product
@@ -22,24 +21,6 @@ def basket_add(request, id):
     else:
         Basket.objects.create(user=user_select, product=product, quantity=1)
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
-
-
-# @login_required
-# def basket_add(request, id):
-#     if request.is_ajax():
-#         user_select = request.user
-#         product = Product.objects.get(id=id)
-#         baskets = Basket.objects.filter(user=user_select, product=product)
-#         if baskets:
-#             basket = baskets.first()
-#             basket.quantity += 1
-#             basket.save()
-#         else:
-#             Basket.objects.create(user=user_select, product=product, quantity=1)
-#         products = Product.objects.all()
-#         context = {'products': products}
-#         result = render_to_string('mainapp/includes/card.html', context)
-#         return JsonResponse({'result': result})
 
 
 @login_required
